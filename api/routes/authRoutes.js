e0cb3d6bdaa0dfc6356fa4df56ace6d705bae06f
santiagobedon <santiago.bedon@correounivalle.edu.co>
2025-09-21 20:40:54 -0500

    solucio  del error del controlador

--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { login, logout, forgotPassword, resetPassword, getProfile } = require("../controllers/authController");
+const { login, logout, forgotPassword, resetPassword, getProfile } = require("../controllers/AuthController");
 const authMiddleware = require("../middlewares/authMiddleware");
 
 // ======================================================
